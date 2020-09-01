917a777ca6dc7cbcdaf9a3b0e0ea2148be29261d
Hai Nguyen <haidnguyen@kms-technology.com>
2020-09-02 00:45:51 +0700

    initial commit

---
+++ jest.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  name: 'angular-unit-test',
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: {
+        before: [
+          'jest-preset-angular/build/InlineFilesTransformer',
+          'jest-preset-angular/build/StripStylesTransformer',
+        ]
+      }
+    },
+  },
+  coverageDirectory: './coverage',
+  snapshotSerializers: [
+    'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
+    'jest-preset-angular/build/AngularSnapshotSerializer.js',
+    'jest-preset-angular/build/HTMLCommentSerializer.js',
+  ],
+};
---
+++ src/app/main/main.component.spec.ts
@@ -0,0 +1,22 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { render, RenderResult } from '@testing-library/angular';
+
+import { MainComponent } from './main.component';
+
+describe('MainComponent', () => {
+  let component: RenderResult<MainComponent>;
+
+  beforeEach(async () => {
+    component = await render(MainComponent);
+  });
+
+  it('should match snapshot', () => {
+    const { container } = component;
+    expect(container).toMatchSnapshot();
+  });
+
+  it('should render correctly', () => {
+    const { getByTestId } = component;
+    expect(getByTestId('test-hello').innerHTML).toEqual('Hello World');
+  });
+});
